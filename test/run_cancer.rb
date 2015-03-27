67c6e5722c7113df893bc7c0681b3623f45c864b
Johnny Lee <jleeothon@gmail.com>
2015-03-27 14:54:14 -0600

    Added .travis.yml

--- test/run_cancer.rb
+++ test/run_cancer.rb
@@ -1,6 +1,5 @@
 load 'baobab.rb'
 
-@dataset = Dataset::from_json('test/breast-cancer-adapted.json')
-@tree = DecisionTree.new @dataset, 'Class'
-puts @tree.to_s
-
+dataset = Dataset::from_json('test/breast-cancer-adapted.json')
+tree = DecisionTree.new dataset, 'Class'
+puts tree.to_s
