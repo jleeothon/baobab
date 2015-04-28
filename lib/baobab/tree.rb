37ee276d251ea526b2efd6dbbb5bdea95804d481
jleeothon <jleeothon@gmail.com>
2015-04-27 22:41:30 -0500

    Corrected bug in argument checking for query It didn't accept parameters in different order. To solve it, have ordered the two arrays (yeah, not too performant, probably). Also, fixed some indentation.

--- lib/baobab/tree.rb
+++ lib/baobab/tree.rb
@@ -5,63 +5,69 @@ module Baobab
 
   class DecisionTree
 
-      #  The first decision tree node
-      attr_reader :root
+    #  The first decision tree node
+    attr_reader :root
 
-      # The class variable of interest in this decision tree.
-      attr_reader :class_var
+    # The class variable of interest in this decision tree.
+    attr_reader :class_var
 
-      # The underlying dataset
-      attr_reader :dataset
+    # The underlying dataset
+    attr_reader :dataset
 
-      def initialize dataset, class_var
-          @dataset = dataset
-          @class_var = class_var
-          entropy = dataset.entropy class_var
-          @root = DecisionTreeNode.new(
-              self, parent=nil,
-              attribute=nil, value=nil,
-              entropy
-              )
-          @root.build_subtree
+    # Receives
+    # - dataset, a list of hashes
+    # - the name of the class variable
+    def initialize dataset, class_var
+      @dataset = dataset
+      if class_var.nil?
+        raise "class_var cannot be nil"
       end
+      @class_var = class_var
+      entropy = dataset.entropy class_var
+      @root = DecisionTreeNode.new(
+        self, parent=nil,
+        attribute=nil, value=nil,
+        entropy
+        )
+      @root.build_subtree
+    end
 
-      # Prints the decision depth-first with the respective entropy values.
-      def to_s
-          s = ""
-          nodes = [[0, @root]]
-          while nodes.any?
-              l, n = nodes.last
-              nodes = nodes.slice(0...-1)
-              n.children.each do |c|
-                  nodes << [l + 1, c]
-              end
-              s = s + "#{' ' * (l * 2)}#{n.to_s}\n"
-          end
-          return s
+    # Prints the decision depth-first with the respective entropy values.
+    def to_s
+      s = ""
+      nodes = [[0, @root]]
+      while nodes.any?
+        l, n = nodes.last
+        nodes = nodes.slice(0...-1)
+        n.children.each do |c|
+            nodes << [l + 1, c]
+        end
+        s = s + "#{' ' * (l * 2)}#{n.to_s}\n"
       end
+      s
+    end
 
-      # Receives attributes and their values (they must be all defined).
-      # Returns the value of the predicted class value.
-      def query values
-          if values.keys != @dataset.attribute_names(@class_var)
-              raise "Query does not fit all variables"
-          end
-          node = @root
-          while node.variable != @class_var
-              if node.next_attribute
-                  if node.children.count > 1
-                      val = values[node.next_attribute]
-                      node = node.children.select do |child|
-                          child.value == val
-                      end[0]
-                  else
-                      node = node.children[0]
-                  end
-              end
-          end
-          node.value
+    # Receives attributes and their values (they must be all defined).
+    # Returns the value of the predicted class value.
+    def query values
+      if values.keys.sort != @dataset.attribute_names(@class_var).sort
+        raise "Query does not fit all variables"
       end
+      node = @root
+      while node.variable != @class_var
+        if node.next_attribute
+          if node.children.count > 1
+            val = values[node.next_attribute]
+            node = node.children.select do |child|
+                child.value == val
+            end[0]
+          else
+            node = node.children[0]
+          end
+        end
+      end
+      node.value
+    end
 
   end
 
